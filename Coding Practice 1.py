e52a444668ced6534497c6ec815a84875a24b141
JamesRooberts <heyjames.roberts@gmail.com>
2023-01-25 20:55:12 -0600

    Homework 1.3

---
+++ Coding Practice 1.py
@@ -0,0 +1,80 @@
+
+# creating variables
+
+floatnumber = 1.5
+
+integernumber = 25
+
+singlequote = 'heyo'
+
+apostrophe = "i'm alive"
+
+# printing variables
+
+print(floatnumber)
+
+print(integernumber)
+
+print(singlequote)
+
+print(apostrophe)
+
+# printing math
+
+print(12635241//2)
+print(1352%33)
+print(5**7)
+print(5**2)
+
+# slicing
+
+numbers = "0123456789"
+
+print(numbers [:1:])
+
+print(numbers [9::])
+
+print(numbers [::2])
+
+print(numbers[::-1])
+
+print(numbers[-1:0:-2])
+
+# Methods
+
+brownfox = "The quick brown fox jumped over the lazy dog"
+
+print(brownfox.upper())
+
+print(brownfox.lower())
+
+print(brownfox.split())
+
+# lists
+
+lst = ['One', 'Two', 'Three']
+
+print(lst[1])
+
+lst[2] = {'key1': 4, 'key2': 'four'}
+
+print(lst)
+
+print(lst[2]['key1'] *2)
+
+print(lst[2]['key2'].upper())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
