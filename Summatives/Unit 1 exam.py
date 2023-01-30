9e5d6e271ee671008eebf0eaf89ecb66978a6517
JamesRooberts <heyjames.roberts@gmail.com>
2023-01-30 15:33:01 -0600

    gfcngcjngcfjn

---
+++ Summatives/Unit 1 exam.py
@@ -0,0 +1,45 @@
+# Write your python code below each comment
+
+# 1. Print “Hello World!” to the console
+
+print("Hello World!")
+
+# 2. Write a multi-line comment
+
+'''this is
+a
+multiline
+comment'''
+
+# 3. Write a single line comment
+
+# This is a single line comment
+
+# 4. Declare a variable named ‘my_var’ to hold this string: “selur nohtyp”
+
+my_var = "selur nohtyp"
+
+# 5. Call the first item in the string my_var and print to the console
+
+print(my_var[0])
+
+# 6. Call the last item in the string my_var and print to the console
+
+print(my_var[11])
+
+# 7. Reverse the string using the string method and print to the console
+
+print(my_var[::-1])
+
+# 8. Declare a variable named ‘my_lst’ to hold this list: [‘first’, ‘second’, ‘third’]
+
+my_lst = ['first', 'second', 'third']
+
+# 9. Call the second item in the list and print to the console
+
+print(my_lst [1])
+
+# 10. Add the string ‘last’ to my_lst and print to the console
+
+my_lst.append("last")
+print(my_lst)
