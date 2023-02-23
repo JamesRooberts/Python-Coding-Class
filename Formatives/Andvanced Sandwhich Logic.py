0eec0b8f499a191fb4e62443e531df2d33a02532
James Roberts <122655637+JamesRooberts@users.noreply.github.com>
2023-02-23 14:23:58 -0600

    Update Andvanced Sandwhich Logic.py

--- Formatives/Andvanced Sandwhich Logic.py
+++ Formatives/Andvanced Sandwhich Logic.py
@@ -1,6 +1,6 @@
 # Sandwhich Paragraph
 
-("to make a ham sandwhich do the following. first, open the fridge door and grab your bread and ham. then, close the fridge door and walk to the cabinet that contains plates. grab a plate, walk to a counter, and set the plate down. then, take two slices of bread out of the bag of bread, and set bag of bread down on the counter. then take 2 slices of ham and put it on top of the left slice of bread. put the container of ham next to the bag of bread on the counter. take the right slice of bread and flip it over ontop of the left slice of bread. then grab the bread bag, and the container of ham and walk to the fridge. lastly, open the fridge and place both items into the fridge. and youre done! ") 
+("to make a ham sandwhich do the following. first, walk to the fridge, and open the door and grab your bread and ham. then, close the fridge door and walk to the cabinet that contains plates. grab a plate, walk to a counter, and set the plate down. then, take two slices of bread out of the bag of bread, and set bag of bread down on the counter. then take 2 slices of ham and put it on top of the left slice of bread. put the container of ham next to the bag of bread on the counter. take the right slice of bread and flip it over ontop of the left slice of bread. then grab the bread bag, and the container of ham and walk to the fridge. lastly, open the fridge and place both items into the fridge. and youre done! ") 
 
 
 # Lesser of two Evens
